57b31b667432bbe9290f51f3c87958bcc81c2979
rachitamaniyar <rachitamaniyar@gmail.com>
2025-03-17 09:43:59 +0100

    Update ersteJavaProgram.java

--- ersteJavaProgram.java
+++ ersteJavaProgram.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 //String and scanner introduction
 public class uebung1 {
-    public static void main(String[] args) {
+    public static void stringAndScanner() {
 
         Scanner firstname = new Scanner(System.in);
         Scanner lastname = new Scanner(System.in);
@@ -20,4 +20,35 @@ public class uebung1 {
         System.out.println("Die länge ist mehr als 10: " + length + " und die exakte länge ist " + name.length());
         System.out.println("Willkommen " + name);
     }
+    //if-else introduction
+   public static void ifElse () {
+        System.out.println("Enter an Integer: ");
+    Scanner num = new Scanner(System.in);
+    int nummer= num.nextInt();
+    if (nummer>5) {
+        System.out.println(nummer +" ist grösser als 5");
+    }
+    else if (nummer<5) {
+        System.out.println(nummer + " ist kleiner als ");
+    }
+    else {
+        System.out.println(nummer +" ist gleich als 5");
+    }
+}
+    //method introduction
+     //public static void main(String[] args) {
+        //name();
+        //wetter();
+       // tier();
+   //}
+    public static void name() {
+        System.out.print("Name: Rachita \n");
+    }
+    public static void wetter() {
+        System.out.print("Wetter: Sonnig \n");
+    }
+    public static void tier() {
+        System.out.print("Hundname: Bunny \n");
+    }
+
 }
