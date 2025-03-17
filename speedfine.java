76003f7a234171ed496c4dc2b4f0a6e0502e234e
rachitamaniyar <rachitamaniyar@gmail.com>
2025-03-17 10:17:32 +0100

    Create speedfine.java

---
+++ speedfine.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class speedfine {
+
+    public static void main(String[] args) {
+        Scanner entry = new Scanner(System.in);
+
+        System.out.println("Enter the speed: ");
+        int geschwindigkeit = entry.nextInt();
+
+        System.out.println("Enter the alcohol level: ");
+        float alk = entry.nextFloat();
+
+        int ueberschreitung;
+        int fine1 = 0;
+        int fine2 = 0;
+
+        if (geschwindigkeit > 50 || alk > 0.5) {
+            ueberschreitung = geschwindigkeit - 50;
+            System.out.println("die überschreitung ist " + ueberschreitung + "km/hr");
+            System.out.println("Die alkoholpegel ist " + alk);
+            //speed fine calculation
+            if (ueberschreitung > 0 && ueberschreitung <= 10) {
+                System.out.println("Sie bekommen eine schriftliche verwarnung");
+            } else if (ueberschreitung > 10 && ueberschreitung <= 20) {
+                fine1 = 30;
+            } else if (ueberschreitung > 20 && ueberschreitung <= 30) {
+                fine1 = 50;
+            } else if (ueberschreitung > 30 && ueberschreitung <= 50) {
+                fine1 = 100;
+            } else if (ueberschreitung > 50 && ueberschreitung <= 100) {
+                fine1 = 500;
+            } else if (ueberschreitung > 100) {
+                fine1 = 1500;
+            } else {
+                System.out.println("Error, measure again.");
+            }
+            //alcohol fine calculation
+            if (alk >= 0.5 && alk < 1.0) {
+                fine2 = 50;
+            } else if (alk >= 1.1 && alk < 2.0) {
+                fine2 = 100;
+            } else if (alk >= 2.0) {
+                fine2 = 2500;
+            } else {
+                System.out.println("No alcohol fine.");
+            }
+            //display fines
+            System.out.println("Die überschreitung strafe: " + fine1 + " EUR");
+            System.out.println("Die Alkohol strafe: " + fine2 + " EUR");
+            System.out.println("Gesamt strafe: " + (fine1 + fine2) + " EUR");
+
+        } else {
+            System.out.println("gute fahrt");
+        }
+        entry.close();
+    }
+}
